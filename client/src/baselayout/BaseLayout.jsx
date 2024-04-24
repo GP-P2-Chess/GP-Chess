6bd5dc6a8253e21f9febf1c27dd8de77c776debc
Axal1s <rizqullahafif@gmail.com>
2024-04-24 11:31:57 +0700

    feat:GamePage Skeleton

--- client/src/baselayout/BaseLayout.jsx
+++ client/src/baselayout/BaseLayout.jsx
@@ -3,10 +3,10 @@ import Navbar from "../components/Navbar";
 
 const BaseLayout = () => {
   return (
-    <>
+    <div className="w-full h-dvh flex flex-col">
       <Navbar />
       <Outlet />
-    </>
+    </div>
   );
 };
 
--- client/src/views/GamePage.jsx
+++ client/src/views/GamePage.jsx
@@ -1,10 +1,32 @@
+import Game from "../components/Game";
+
 const GamePage = () => {
   return (
-    <>
-      <div>
-        <h1>Ini GamePage</h1>
+    <section className="w-full grow flex flex-row justify-center items-center">
+      <div className="w-1/4 h-full flex flex-col justify-end items-center pb-12">
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <p>Player Name</p>
+          <p>Win Rate</p>
+          <p>MMR</p>
+        </div>
       </div>
-    </>
+      <div className="w-2/5">
+        <Game />
+      </div>
+      <div className="w-1/4 h-full flex flex-col justify-start items-center pt-12 ">
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <p>Opponent Name</p>
+          <p>Win Rate</p>
+          <p>MMR</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
