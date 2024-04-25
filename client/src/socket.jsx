876c0416e2735ceca0039800d482af081dfb41ea
Axal1s <rizqullahafif@gmail.com>
2024-04-25 15:04:18 +0700

    changed url to domain

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"; // import connection function
 
-const socket = io("https://34.124.195.26", { autoConnect: false }); // initialize websocket connection
+const socket = io("https://chess.saintmichael.cloud", { autoConnect: false }); // initialize websocket connection
 
 export default socket;
